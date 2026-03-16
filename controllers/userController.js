9770ca30dc2f44d313615f79aeb26b30a2506b29
supreMe7492 <supremeraut94@gmail.com>
2026-03-16 16:40:51 +0545

    added jwt token after login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
-const {addAuthor} = require("../db/query");
+const {addAuthor,selectUser} = require("../db/query");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const bcrypt = require('bcryptjs');
+
 async function signUser(req,res) {
     const name = req.body.name;
     const password = await bcrypt.hash(req.body.password,10);
@@ -8,4 +11,17 @@ async function signUser(req,res) {
 
 }
 
-module.exports = {signUser}
+async function logUser(req,res){
+    const username = req.body.name;
+    const password = req.body.password;
+    const user = selectUser(username);
+    if(!user)
+        return res.json("no username");
+    const match = await bcrypt.compare(password,user.password);
+    if(!match)
+        return res.json("incorrect password");
+    const token = jwt.sign({id:user.id,name:user.name},process.env.secretkey,{ expiresIn: '7d' })
+    res.json(token);   
+}
+
+module.exports = {signUser,logUser};
